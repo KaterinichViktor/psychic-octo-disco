46b16aa3c959e80578baefd9454fe83aa4b6187e
KaterinichViktor <vspasedust@gmail.com>
2023-05-02 18:57:52 +0300

    ...

--- js/account-type-btn.js
+++ js/account-type-btn.js
@@ -1,26 +1,3 @@
-// const selectBtn = document.querySelector('.select-btn');
-// const selectDropdown = document.querySelector('.select-dropdown');
-// let selectedOption = '';
-
-// selectBtn.addEventListener('click', () => {
-//   selectDropdown.style.display = selectDropdown.style.display === 'none' ? 'block' : 'none';
-// });
-
-// selectDropdown.addEventListener('click', (event) => {
-//   const button = event.target.closest('button');
-//   if (button) {
-//     console.log(button.innerText); // do something with the selected option
-//     selectedOption = button.innerText;
-//     selectBtn.innerText = selectedOption;
-//     selectDropdown.style.display = 'none';
-//   }
-// });
-
-// document.addEventListener('click', (event) => {
-//   if (!selectBtn.contains(event.target) && !selectDropdown.contains(event.target)) {
-//     selectDropdown.style.display = 'none';
-//   }
-// });
 
 const selectBtn = document.querySelector('.select-btn');
 const selectDropdown = document.querySelector('.select-dropdown');
